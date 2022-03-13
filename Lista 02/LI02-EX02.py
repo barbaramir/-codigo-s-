876ed5c1bea3b41bf9fe71e38a36598ba5dca4e8
Bárbara <101302079+barbaramir@users.noreply.github.com>
2022-03-13 19:40:09 -0300

    Add files via upload

---
+++ Lista 02/LI02-EX02.py
@@ -0,0 +1,10 @@
+"""
+Exercício 2:
+Crie uma lista com 10 elementos (você escolhe quais serão) e imprima a lista na ordem inversa. Exemplo: para a lista [1, 3, 6, “H”, 
+[7,7,7] a saída deve ser >>> [[7,7,7], “H”, 6, 3, 1]
+"""
+variaveis = ["Julia", 257, 8.3698, [-4, 2, 0, 12], (1990, 1992, 1998), "Lis", 58.63, ["Lu", "La"], (12, 24), 2222]
+
+inverso = variaveis.reverse()
+
+print(variaveis)
